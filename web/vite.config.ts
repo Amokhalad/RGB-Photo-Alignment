8a52af84102e9047910615c6036de5c127416a5e
Mokhalad <mjayalju12234@gmail.com>
2024-09-10 13:03:47 -0700

    image loading fix attempt 1

--- web/vite.config.ts
+++ web/vite.config.ts
@@ -4,5 +4,4 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: './', // This makes all URLs relative to the current folder
 });
